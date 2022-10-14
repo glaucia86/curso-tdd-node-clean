f08c6347edaa6cd51d0c188b429bdefd98f75984
glaucia86 <glaucia_lemos86@hotmail.com>
2022-10-14 20:36:36 -0300

    chore: some important changes in the configuration setup

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,24 @@
+/**
+ * file: src/presentation/controllers/signup.spect.ts
+ * date: 10/14/2022
+ * description: file responsible for the 'SignUp' function tests
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+import { SignUpController } from './signup';
+
+describe('Signup Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  })
+})
---
+++ src/presentation/controllers/signup.ts
@@ -0,0 +1,12 @@
+/**
+ * file: src/presentation/controllers/signup.ts
+ * date: 10/14/2022
+ * description: file responsible for the 'SignUp' function
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+export class SignUpController {
+  handle(httpRequest: any): any {
+
+  }
+}
