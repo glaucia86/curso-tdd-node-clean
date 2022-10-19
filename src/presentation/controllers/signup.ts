545f5edec6c607573f8303a602e28938698bde35
glaucia86 <glaucia_lemos86@hotmail.com>
2022-10-18 21:39:38 -0300

    refactor: change the way validate code in 'signup.ts' file

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -11,12 +11,12 @@ import { badRequest } from '../helpers/http-helper';
 
 export class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'));
-    }
+    const requiredFields = ['name', 'email'];
 
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'));
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field));
+      }
     }
   }
 }
