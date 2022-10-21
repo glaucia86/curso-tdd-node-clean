5dae2f2d2ea853f2380b31e9f56a60bb0d5b8aa4
glaucia86 <glaucia_lemos86@hotmail.com>
2022-10-21 12:28:59 -0300

    refactor: move server error to a helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -8,8 +8,7 @@
 import { HttpResponse, HttpRequest } from '../protocols/http';
 import { MissingParamError } from '../errors/missing-param-error';
 import { InvalidParamError } from '../errors/invalid-param-error';
-import { ServerError } from './../errors/server-error';
-import { badRequest } from '../helpers/http-helper';
+import { badRequest, serverError } from '../helpers/http-helper';
 import { Controller } from '../protocols/controller';
 import { EmailValidator } from '../protocols/email-validator';
 
@@ -36,10 +35,7 @@ export class SignUpController implements Controller {
         return badRequest(new InvalidParamError('email'));
       }
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: new ServerError()
-      }
+      return serverError();
     }
   }
 }
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -5,9 +5,15 @@
  * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
  */
 
+import { ServerError } from '../errors/server-error';
 import { HttpResponse } from '../protocols/http';
 
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
+});
+
+export const serverError = (): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError()
 });
