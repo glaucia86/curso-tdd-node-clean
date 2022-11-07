f355b10ee46d9aca4c823ea079fd31daf172bdb2
glaucia86 <glaucia_lemos86@hotmail.com>
2022-11-07 14:59:16 -0300

    test: ensure 'SignupController' returns status code 500 if 'AddAccount' throws error

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -189,6 +189,27 @@ describe('Signup Controller', () => {
     expect(httpResponse.body).toEqual(new ServerError());
   })
 
+  test('Should return 500 if AddAccount throws', () => {
+
+    const { sut, addAccountStub } = makeSut();
+    jest.spyOn(addAccountStub, 'add').mockImplementationOnce(() => {
+      throw new Error
+    });
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual(new ServerError());
+  })
+
   test('Should call AddAccount with correct values', () => {
 
     const { sut, addAccountStub } = makeSut();
