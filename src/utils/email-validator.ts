72f6cfccbec211bd6ff0255b16471c071c3209c8
glaucia86 <glaucia_lemos86@hotmail.com>
2022-11-10 20:51:32 -0300

    feat: ensure 'EmailValidatorAdapter' returns false if validator returns false

---
+++ src/utils/email-validator.ts
@@ -0,0 +1,14 @@
+/**
+ * file: src/utils/email-validator.ts
+ * date: 11/10/2022
+ * description: file responsible for validating the email
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+import { EmailValidator } from "../presentation/protocols/email-validator";
+
+export class EmailValidatorAdapter implements EmailValidator {
+  isValid(email: string): boolean {
+    return false;
+  }
+}
